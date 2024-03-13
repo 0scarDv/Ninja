cf8b18833cc01273f1d3bef982b1ba74371a8bc4
0scarDv <152935162+0scarDv@users.noreply.github.com>
2024-03-13 18:20:38 -0300

    Add files via upload

---
+++ ninja.js
@@ -0,0 +1,22 @@
+class ninja {
+    constructor(nombre, salud, velocidad = 3, fuerza = 3,) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.velocidad = velocidad;
+        this.fuerza = fuerza;
+    }
+    sayName() {
+        console.log("Mi nombre es: " + this.nombre);
+    }
+
+    showStats() {
+        console.log("Nombre:" + this.nombre);
+        console.log("Fuerza: " + this.fuerza);
+        console.log("Velocidad: " + this.velocidad);
+        console.log("Salud: " + this.salud);
+    }
+    drinkSake() {
+        this.salud += 10;
+    }
+
+}
